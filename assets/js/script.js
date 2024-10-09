a29de8d2eebee691a28881217d4a19e98d507ae4
Uche05 <uchekpadeuwa@gmail.com>
2024-10-09 18:42:12 +0100

    add comments of what to write scripts for

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// lets start by making a question set which contains the answers as well
+
+//remember that based on how your html structure is we need to remove the instruction-set before we start questions
+//note that you will need to show and hide certain elements at certain times and when certain things occur
+
+//we need a function that displays the question
+
+//we need a function that checks the answer and tracks the user's score
+
+// we need a function that tracks how many questions the user has made and to ensure the game stops after a certain number of questions have been answered
+
+// a function that can make an analysis section after user has opted to check the analytics
